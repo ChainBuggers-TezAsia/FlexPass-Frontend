fb24e597cf45852a70f70fb611726e7d7bd5ee70
vk2122 <varunkhachane31@gmail.com>
2023-08-18 03:47:36 +0530

    Footer Added

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,16 +8,13 @@ export default function LandingPage() {
   return (
     <div>
       <Header />
-      {/* <div className="z-10">
-        kvnrn
-      </div> */}
       <div className="absolute top-0 " style={{ zIndex: -2 }}>
         <img className="z-0 w-screen" src={bgImg} alt="" />
       </div>
       <div>
-        <CardBase/>
+        <CardBase />
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
