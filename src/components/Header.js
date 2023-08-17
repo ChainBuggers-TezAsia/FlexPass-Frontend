ef3e2ac1647913ace4faf1415ee5569739d65644
vk2122 <varunkhachane31@gmail.com>
2023-08-18 02:50:34 +0530

    Header Created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="relative w-full h-[6rem] overflow-hidden text-center text-[1rem] text-white font-inter">
+    <header className="relative w-full h-[6rem] overflow-hidden text-white font-inter">
       <div className="absolute inset-0 w-full h-[6.06rem]">
         <img
           className="absolute inset-0 w-full h-[6rem] object-cover"
@@ -9,20 +9,26 @@ const Header = () => {
         />
         <div className="absolute inset-0 bg-gray-200 backdrop-blur-[7.2px] opacity-50 border border-solid border-gray-100" />
       </div>
-      <nav className="absolute top-4 right-4 space-x-4">
-        <button className="px-4 py-2 rounded-md bg-blue-600 text-white font-medium hover:bg-blue-700">
-          Login
-        </button>
-        <button className="px-4 py-2 rounded-md bg-blue-600 text-white font-medium hover:bg-blue-700">
-          Start now
-        </button>
-      </nav>
       <h1 className="absolute top-2 left-12 text-2xl font-extrabold font-poppins">
         FlexPass
       </h1>
-      <nav className="absolute top-4 left-36 space-x-4">
-        <button className="text-white hover:text-gray-300">My Tickets</button>
-        <button className="text-white hover:text-gray-300">Wallet</button>
+      <nav className="absolute top-10 left-72 space-x-4">
+        <a className="text-white hover:text-gray-300">My Tickets</a>
+        <a className="text-white hover:text-gray-300">Wallet</a>
+      </nav>
+      <nav className="absolute top-5 right-7 space-x-4 flex flex-row">
+        <div className="flex justify-center items-center relative h-75 w-200 rounded-full border border-solid border-white">
+          <div className="absolute inset-0 bg-opacity-0 bg-white rounded-full border border-solid border-white"></div>
+          <span className="text-base font-medium text-white z-10 px-8 py-2">
+            Login
+          </span>
+        </div>
+        <div className="flex justify-center items-center relative h-75 w-200 rounded-full bg-[#6851FF]">
+          <div className="absolute inset-0 bg-opacity-0 bg-white rounded-full border border-solid border-[#6851FF]"></div>
+          <span className="text-base font-medium text-white z-10 px-8 py-3">
+            Start now
+          </span>
+        </div>
       </nav>
     </header>
   );
