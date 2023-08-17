5e88e853f69fd784ff7f449d25615780cdf1b5af
Varun Khachane <varunkhachane31@gmail.com>
2023-08-18 04:54:35 +0530

    Hero Section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <div className="flex flex-row">
+      <div className="text-red-500"> 
+        Unforgettable era of entertainment through our gateway to exceptional
+        shows and experiences!
+        <div>Button</div>
+      </div>
+      <div>
+        <img src="../MobileTickets.png" alt="" />
+      </div>
+    </div>
+  );
+}
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import bgImg from "../assets/bg.svg";
 import CardBase from "../components/CardBase";
 import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
 
 export default function LandingPage() {
   return (
@@ -11,15 +12,7 @@ export default function LandingPage() {
       <div className="absolute top-0 " style={{ zIndex: -2 }}>
         <img className="z-0 w-screen" src={bgImg} alt="" />
       </div>
-      <div className="flex flex-row">
-        <div>
-        Unforgettable era of entertainment through our gateway to exceptional shows and experiences!
-        <div>Button</div>
-        </div>
-        <div>
-          <img src="/MobileTickets" alt="" />
-        </div>
-      </div>
+      <HeroSection />
       <div>
         <CardBase />
       </div>
