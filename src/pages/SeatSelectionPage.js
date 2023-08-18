bdc73216b8301c4a74376262556a0f5d449ab54b
Wade <hhh@example.com>
2023-08-19 04:48:40 +0530

    .

--- src/pages/SeatSelectionPage.js
+++ src/pages/SeatSelectionPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import bgImg from "../assets/bg.svg";
-// import Header from "../components/Header";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function SeatSelectionPage() {
