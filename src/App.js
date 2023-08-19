b6d8eb064b7a5262fb4670eb5a866044b018c114
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2023-08-20 02:33:58 +0530

    router

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Login from "./components/Login";
 // import CardBase from "./components/CardBase";
-//import TheatreSelectionPage from "./pages/TheatreSelectionPage";
+// import TheatreSelectionPage from "./pages/TheatreSelectionPage";
 // import TheatreSelectionPage from "./pages/TheatreSelectionPage";
 import LandingPage from "./pages/LandingPage";
 // import MovieCard from "./components/Movie";
-// import SeatSelectionPage from "./pages/SeatSelectionPage";
-import MovieBanner from '../src/components/MovieBanner'
+import SeatSelectionPage from "./pages/SeatSelectionPage";
+import MovieBanner from "../src/components/MovieBanner";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import TheatreSelectionPage from "./pages/TheatreSelectionPage";
 
 function App() {
   const [login, setLogin] = useState(false);
 
   return (
     <div className="App">
-
-      <MovieBanner />
-
+      {/* <Navbar /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route
+            exact
+            path="/theatreSelection"
+            element={<TheatreSelectionPage />}
+          />
+          <Route exact path="/seatSelection" element={<SeatSelectionPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
