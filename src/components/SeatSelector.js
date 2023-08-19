ac299a5e04bc112e958c444cd45af1cbecdba9bd
Wade <hhh@example.com>
2023-08-20 02:29:46 +0530

    ....

--- src/components/SeatSelector.js
+++ src/components/SeatSelector.js
@@ -1,69 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import './SeatSelector.css'; // Import the CSS file for this component
 
-const MovieSeatBooking = () => {
+const SeatSelector = () => {
   const [selectedSeats, setSelectedSeats] = useState([]);
   const [ticketPrice, setTicketPrice] = useState(10); // Default ticket price
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
-    // Load data from local storage if available
-    const savedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
-    if (savedSeats) {
-      setSelectedSeats(savedSeats);
-    }
-  }, []);
-
-  useEffect(() => {
-    // Save selected seats to local storage
-    localStorage.setItem("selectedSeats", JSON.stringify(selectedSeats));
+    updateSelectedCount();
   }, [selectedSeats]);
 
-  const handleSeatClick = (seatIndex) => {
-    setSelectedSeats((prevSelectedSeats) => {
-      if (prevSelectedSeats.includes(seatIndex)) {
-        return prevSelectedSeats.filter((seat) => seat !== seatIndex);
+  // Handle seat selection
+  const handleSeatClick = (e) => {
+    const seatIndex = parseInt(e.target.dataset.seatIndex, 10);
+    if (!isNaN(seatIndex)) {
+      if (!selectedSeats.includes(seatIndex)) {
+        setSelectedSeats([...selectedSeats, seatIndex]);
       } else {
-        return [...prevSelectedSeats, seatIndex];
+        setSelectedSeats(selectedSeats.filter((index) => index !== seatIndex));
       }
-    });
-  };
-
-  const renderSeats = () => {
-    const totalSeats = 40; // Total number of seats
-    const seats = [];
-
-    for (let i = 0; i < totalSeats; i++) {
-      const isOccupied = selectedSeats.includes(i);
-      const isSelected = selectedSeats.includes(i);
-
-      seats.push(
-        <div
-          key={i}
-          className={`seat ${isOccupied ? "occupied" : ""} ${
-            isSelected ? "selected" : ""
-          }`}
-          onClick={() => handleSeatClick(i)}
-        ></div>
-      );
     }
-
-    return seats;
   };
 
-  const calculateTotalPrice = () => {
-    return selectedSeats.length * ticketPrice;
+  // Calculate and update selected seats count and total price
+  const updateSelectedCount = () => {
+    const selectedSeatsCount = selectedSeats.length;
+    setTotalPrice(selectedSeatsCount * ticketPrice);
   };
 
   return (
-    <div className="seat-booking">
-      <div className="screen"></div>
-      <div className="row">{renderSeats()}</div>
-      <div className="total-price">
-        Total Seats Selected: {selectedSeats.length}
-        <br />
-        Total Price: ${calculateTotalPrice()}
+    <div className="seat-selector">
+      <h1>Select Your Seats</h1>
+      <div className="container">
+        {Array.from({ length: 6 }).map((_, rowIndex) => (
+          <div className="row" key={rowIndex}>
+            {Array.from({ length: 8 }).map((_, seatIndex) => {
+              const seatNumber = rowIndex * 8 + seatIndex + 1;
+              const isOccupied = seatNumber === 10 || seatNumber === 25 || seatNumber === 42;
+              const isSelected = selectedSeats.includes(seatNumber);
+              const seatClassNames = `seat ${isOccupied ? 'occupied' : ''} ${isSelected ? 'selected' : ''}`;
+              return (
+                <div
+                  className={seatClassNames}
+                  key={seatIndex}
+                  data-seat-index={seatNumber}
+                  onClick={handleSeatClick}
+                />
+              );
+            })}
+          </div>
+        ))}
+        <div className="screen" />
+      </div>
+      <p className="text">
+        You have selected <span id="count">{selectedSeats.length}</span> seats for a price of $
+        <span id="total">{totalPrice}</span>
+      </p>
+      <div className="footer">
+        <p>Seat Selector</p>
       </div>
     </div>
   );
 };
 
-export default MovieSeatBooking;
+export default SeatSelector;
