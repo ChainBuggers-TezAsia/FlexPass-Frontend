ebb019dcc3231bbf639bed6a0c54179590a57c6e
Wade <hhh@example.com>
2023-08-20 03:29:18 +0530

    .

--- src/components/SeatSelector.js
+++ src/components/SeatSelector.js
@@ -1,64 +1,43 @@
-import React, { useState, useEffect } from 'react';
-// import './SeatSelector.css'; // Import the CSS file for this component
+import React, { useState } from "react";
+import "../SeatSelector.css"; // Import your CSS file for styling
 
 const SeatSelector = () => {
   const [selectedSeats, setSelectedSeats] = useState([]);
-  const [ticketPrice, setTicketPrice] = useState(10); // Default ticket price
-  const [totalPrice, setTotalPrice] = useState(0);
 
-  useEffect(() => {
-    updateSelectedCount();
-  }, [selectedSeats]);
-
-  // Handle seat selection
-  const handleSeatClick = (e) => {
-    const seatIndex = parseInt(e.target.dataset.seatIndex, 10);
-    if (!isNaN(seatIndex)) {
-      if (!selectedSeats.includes(seatIndex)) {
-        setSelectedSeats([...selectedSeats, seatIndex]);
-      } else {
-        setSelectedSeats(selectedSeats.filter((index) => index !== seatIndex));
-      }
+  const handleSeatClick = (seat) => {
+    if (selectedSeats.includes(seat)) {
+      setSelectedSeats(selectedSeats.filter((selectedSeat) => selectedSeat !== seat));
+    } else {
+      setSelectedSeats([...selectedSeats, seat]);
     }
-  };
-
-  // Calculate and update selected seats count and total price
-  const updateSelectedCount = () => {
-    const selectedSeatsCount = selectedSeats.length;
-    setTotalPrice(selectedSeatsCount * ticketPrice);
   };
 
   return (
     <div className="seat-selector">
-      <h1>Select Your Seats</h1>
-      <div className="container">
-        {Array.from({ length: 6 }).map((_, rowIndex) => (
-          <div className="row" key={rowIndex}>
+      <h2>Select Your Seats</h2>
+      <div className="seat-grid">
+        {Array.from({ length: 5 }).map((_, rowIndex) => (
+          <div className="seat-row" key={rowIndex}>
             {Array.from({ length: 8 }).map((_, seatIndex) => {
               const seatNumber = rowIndex * 8 + seatIndex + 1;
-              const isOccupied = seatNumber === 10 || seatNumber === 25 || seatNumber === 42;
-              const isSelected = selectedSeats.includes(seatNumber);
-              const seatClassNames = `seat ${isOccupied ? 'occupied' : ''} ${isSelected ? 'selected' : ''}`;
+              const isSeatSelected = selectedSeats.includes(seatNumber);
+              const seatClass = isSeatSelected ? "seat selected" : "seat";
               return (
                 <div
-                  className={seatClassNames}
                   key={seatIndex}
-                  data-seat-index={seatNumber}
-                  onClick={handleSeatClick}
-                />
+                  className={seatClass}
+                  onClick={() => handleSeatClick(seatNumber)}
+                >
+                  {seatNumber}
+                </div>
               );
             })}
           </div>
         ))}
-        <div className="screen" />
       </div>
-      <p className="text">
-        You have selected <span id="count">{selectedSeats.length}</span> seats for a price of $
-        <span id="total">{totalPrice}</span>
+      <p className="selected-seats">
+        Selected Seats: {selectedSeats.join(", ")}
       </p>
-      <div className="footer">
-        <p>Seat Selector</p>
-      </div>
     </div>
   );
 };
--- src/pages/SeatSelectionPage.js
+++ src/pages/SeatSelectionPage.js
@@ -1,27 +1,13 @@
 import React from "react";
-import bgImg from "../assets/bg.svg";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import SeatSelector from "../components/SeatSelector"; // Import the MovieSeatBooking component
+import SeatSelector from "../components/SeatSelector";
 
-export default function SeatSelectionPage() {
+function SeatSelectionPage() {
   return (
-    <div className="relative min-h-screen overflow-x-hidden">
-      {/* Background Image */}
-      <img className="absolute top-0 w-full h-full object-cover z-0" src={bgImg} alt="" />
-
-      {/* Content */}
-      <div className="relative z-10">
-        {/* <Header /> */}
-        <main className="container mx-auto mt-10 p-4">
-          <h1 className="text-3xl font-semibold mb-4">Select Your Seats</h1>
-          {/* Render the MovieSeatBooking component */}
-          <SeatSelector />
-        </main>
-      </div>
-      <div className="absolute bottom-0 w-full">
-        {/* <Footer /> */}
-      </div>
+    <div>
+      <h1>Book Your Seats</h1>
+      <SeatSelector />
     </div>
   );
 }
+
+export default SeatSelectionPage;
