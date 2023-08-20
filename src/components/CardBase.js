81055a2c65857e0323113f9dbee3f0f94326d400
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2023-08-20 05:33:04 +0530

    movies poster to page

--- src/components/CardBase.js
+++ src/components/CardBase.js
@@ -1,19 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from 'react-router-dom';
 import MovieCard from "./Movie";
 export default function CardBase() {
+  const navigate = useNavigate();
+
+  const [movies, setMovies] = useState([]);
+  const x =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGRhOGFhNTk5NjRlMGRlYWMyZDU1NzIiLCJpYXQiOjE2OTI0ODg3NDMsImV4cCI6MTY5MjQ5MjM0M30.-lUvKPStId5c7E5_BO4PkkggNI0L9L0cEiZIdqQucaI";
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `https://flexpass-back.onrender.com/movie/getAllMovies`,
+      headers: {
+        Authorization: `Bearer ${x}`,
+      },
+    })
+      .then((response) => {
+        // setSplitInto(response.data.userNames)
+        console.log("data", response.data);
+
+        setMovies(response.data);
+        console.log("state", movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [0]);
+
   return (
-    // <div className="text-white  left-[7.69rem] text-[3rem] tracking-[0.05em] capitalize font-semibold inline-block w-[40.44rem] h-[5.88rem]">
-      // Movies
-      <div className="text-white capitalize font-semibold  text-[4rem]">
-        <span className="p-20">Movies</span>
-        
-        <div className="grid grid-cols-3 justify-items-center ">
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-        </div>
+    <div className="text-white capitalize font-semibold  text-[4rem]">
+      <span className="p-20">Movies</span>
+
+      <div className="grid grid-cols-3 justify-items-start ">
+        {movies.map((movie) => {
+          return (
+            <div onClick={()=>{
+              navigate('/theatreSelection',{state:movie});
+            }}>
+              {/* <Link
+                to={{
+                  pathname: "/theatreSelection",
+                  state: {id:1, name:"name"} ,
+                }}
+              > */}
+                <MovieCard
+                  name={movie.name}
+                  image={movie.image}
+                  release={movie.release}
+                />
+              {/* </Link> */}
+            </div>
+          );
+        })}
+
+        {/* <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard /> */}
+      </div>
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,25 @@
-const MovieCard = () => {
+const MovieCard = (props) => {
+  const inputDate = new Date(props.release);
+  const day = String(inputDate.getDate()).padStart(2, "0");
+  const month = String(inputDate.getMonth() + 1).padStart(2, "0");
+  const year = inputDate.getFullYear();
   return (
-    <div className="relative rounded-sm w-full h-[27.13rem] overflow-hidden text-left text-[1.56rem] text-white font-oxanium m-20 pb-4">
-      <div className="absolute top-[0rem] left-[0rem] w-[18.19rem] h-[27.13rem] ml-24">
-        <div className="absolute top-[0rem] left-[0rem] w-[18.19rem] h-[27.13rem]">
-          <div className="absolute top-[0rem] left-[0rem] rounded-mini [background:linear-gradient(147.75deg,_rgba(255,_255,_255,_0.1),_rgba(255,_255,_255,_0.05))] [backdrop-filter:blur(20px)] w-[18.19rem] h-[27.13rem] pb-4" />
-          <div className="absolute top-[1.5rem] left-[1.5rem] w-[15.19rem] h-[24.13rem]">
+    <div className="relative rounded-sm w-full h-[27.13rem] overflow-hidden text-left text-[1.56rem] text-white font-oxanium m-[9rem] pb-4">
+      <div className="absolute top-[0rem] left-[0rem] w-[18.19rem] h-[32rem] ">
+        <div className="absolute top-[0rem] left-[0rem] w-[18.19rem] h-[32rem]">
+          <div className="absolute top-[0rem] left-[0rem] rounded-mini [background:linear-gradient(147.75deg,_rgba(255,_255,_255,_0.1),_rgba(255,_255,_255,_0.05))] [backdrop-filter:blur(20px)] w-[18.19rem] h-[32rem] pb-4" />
+          <div className="absolute top-[1.5rem] left-[1.5rem] w-[15.19rem] h-[32rem]">
             <img
               className="absolute top-[0rem] left-[0rem] rounded-mini w-[15.19rem] h-[19.63rem] object-cover"
               alt=""
-              src="/nft-picture@2x.png"
+              src={props.image}
             />
             <div className="absolute top-[20.94rem] left-[0rem] w-[11.31rem] flex flex-col items-start justify-start gap-[0.19rem]">
               <div className="relative tracking-[0.05em] capitalize">
-                Spider Man
+                {props.name}
               </div>
               <div className="relative text-[0.88rem] tracking-[0.05em] capitalize font-lato">
-                Release Date: 2 June 2023
+                {`${day}-${month}-${year}`}
               </div>
             </div>
             <div className="absolute top-[20.94rem] left-[11.25rem] w-[3.5rem] h-[1.25rem] text-[0.75rem]">
