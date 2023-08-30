212b719c14a896cc9f8be5008603d803194c81d1
Varun Khachane <varunkhachane31@gmail.com>
2023-08-31 00:06:01 +0530

    Version matching

--- src/components/MyWallet.js
+++ src/components/MyWallet.js
@@ -1,61 +1,122 @@
-import React from 'react'
-import { TezosToolkit } from "@taquito/taquito";
-import { TempleWallet } from '@temple-wallet/dapp';
-import TxnActivity from './TxnActivity';
-
-const Tezos = new TezosToolkit('https://testnet-tezos.giganode.io');
+import React, { useState } from "react";
+import Web3 from "web3";
+import axios from "axios";
+import { FaCopy } from "react-icons/fa6";
+import WalletLog from "./WalletLog";
 
 export default function UserDetails(props) {
-  const handleTicket = () => {
-    props.setButton(1);
+  const [active, setActive] = useState(0);
+  const [sideBarActive, setSideBarActive] = useState(true);
+  const [address, setAddress] = useState();
+
+  const toggleActive = () => {
+    setSideBarActive(!sideBarActive);
+  };
+
+  const changeActive = (index) => {
+    if (index === active) {
+      setActive(0);
+    } else {
+      setActive(index);
+    }
   };
 
   const handleConnectWallet = async () => {
-    try {
-      const available = await TempleWallet.isAvailable();
-      if (!available) {
-        console.log("Temple Wallet is not available");
-        return;
+    const x = localStorage.getItem("jwt_token");
+
+    if (window.ethereum) {
+      try {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        console.log("Connected to Metamask");
+        window.ethereum
+          .enable()
+          .then(async (accounts) => {
+            const userWalletAddress = accounts[0];
+            setAddress(userWalletAddress);
+            console.log("User Wallet Address:", userWalletAddress);
+            const balanceWei = await web3.eth.getBalance(userWalletAddress);
+            const balanceEth = web3.utils.fromWei(balanceWei, "ether");
+            console.log("Account Balance (Ether):", balanceEth);
+            const token = JSON.parse(x).token
+            axios({
+              method: "post",
+              // url: "https://flexpass-back.onrender.com/user/login",
+              url: `https://flexpass-back.onrender.com/user/addAddress/${userWalletAddress}`,
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+              // data: { email: email, password: password.toString() },
+            })
+              // .then(function (response) {
+              //   console(response);
+              // })
+              // .catch(function (error) {
+              //   console.log(error);
+              // });
+            // console.log("ress",data);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+
+        // Now you can use the 'web3' instance to interact with the Ethereum network
+      } catch (error) {
+        console.log(error);
       }
-
-      const wallet = new TempleWallet("Flexpass");
-      await wallet.connect("ghostnet");
-
-      Tezos.setWalletProvider(wallet);
-
-      const pkh = await wallet.getPKH();
-      console.log(`Connected with address: ${pkh}`);
-    } catch (error) {
-      console.error("Error connecting wallet:", error);
+    } else {
+      console.log("Please install Metamask");
     }
   };
 
   return (
-    <div className="mx-5 my-5">
-      <div className="relative rounded-lg px-5 py-3 [background:linear-gradient(rgba(0,_0,_0,_0.14),_rgba(0,_0,_0,_0.14)),_linear-gradient(-38.77deg,_rgba(191,_191,_191,_0.06),_rgba(0,_0,_0,_0))] shadow-[-8px_4px_5px_rgba(0,_0,_0,_0.24)] [backdrop-filter:blur(53px)] w-full h-52 font-poppins">
-        <div className='relative flex  items-center justify-between'>
-          <b className="text-[1.75rem] text-white text-left">
-            My Wallet
-          </b>
-          <button
-            className="flex justify-center items-center relative h-75 w-200 rounded-full bg-[#6851FF]"
-            onClick={() => {
-              handleConnectWallet()
-            }}
-          >
-            <div className="absolute inset-0 bg-opacity-0 bg-white rounded-full border border-solid border-[#6851FF]"></div>
-            <span className="text-base font-medium text-white z-10 px-8 py-3">
-              +Add Wallet
-            </span>
-          </button>
+    <div className="w-full my-10">
+      <div className="flex flex-col">
+        <div className="font-poppins font-bold text-white text-[28px] mx-14 mb-8">My Wallet : </div>
+        <div className="flex justify-around items-center">
+          <div className="relative rounded-[20px] [background:linear-gradient(rgba(0,_0,_0,_0.14),_rgba(0,_0,_0,_0.14)),_linear-gradient(-38.77deg,_rgba(191,_191,_191,_0.06),_rgba(0,_0,_0,_0))] shadow-[-8px_4px_5px_rgba(0,_0,_0,_0.24)] [backdrop-filter:blur(53px)] w-1/2 h-[11rem] text-center text-lg text-white font-montserrat py-6 px-4 ml-12 mr-12">
+            <div className="flex flex-col">
+              <div className="relative text-3xl montserrat text-white text-left opacity-[0.6] mb-2">Total Balance</div>
+              <b className="relative text-21xl font-manrope text-white text-left mb-5">Rs. 1910.00</b>
+              <div className="mx-1 my-2">
+                <div className="flex items-center justify-around">
+                  <div className="relative rounded-[20px] box-border w-3/4 h-6 border-[0.5px] border-solid border-white items-center">
+                    <div className="flex justify-around items-center">
+                      <div className="relative text-sm overflow-hidden"><FaCopy /></div>
+                      <div className="relative text-[0.95rem] font-light font-montserrat text-white text-center">0xfb7b2ad98f9e622b487c582b080cbf4bcfa2b08f</div>
+                    </div>
+                  </div>
+                  <button
+                    className="ml-2 flex justify-center items-center relative h-8 w-1/4 rounded-full bg-purple-800"
+                    onClick={handleConnectWallet}
+                  >
+                    <div className="font-medium font-poppins text-white text-center text-base">+Add Money</div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative rounded-[20px] [background:linear-gradient(rgba(0,_0,_0,_0.14),_rgba(0,_0,_0,_0.14)),_linear-gradient(-38.77deg,_rgba(191,_191,_191,_0.06),_rgba(0,_0,_0,_0))] shadow-[-8px_4px_5px_rgba(0,_0,_0,_0.24)] [backdrop-filter:blur(53px)] w-1/2 h-[11rem] text-center text-lg text-white font-montserrat py-6 px-4 mr-12">
+            <div className="flex flex-col">
+              <div className="relative text-3xl montserrat text-white text-left opacity-[0.6]">Total Purchase</div>
+              <div className="flex justify-center items-center">
+                <div className="flex flex-col justify-center items-center mr-16">
+                  <img src='/2.svg' />
+                  <div className="relative text-base font-poppins text-white text-center">Resell</div>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                  <img src='/1.svg' />
+                  <div className="relative text-base font-poppins text-white text-center">Sold</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>
-      <div className="mt-10 rounded-lg bg-[#242333] py-10 px-20">
-        <div className='h-20 text-white'>
-            Wallet Activity
+        <div className="mt-6 mx-12 mb-2 rounded-t-lg">
+          <WalletLog />
         </div>
-        <TxnActivity/>
       </div>
     </div>
-  )
+  );
 }
--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -30,17 +30,21 @@ export default function OrderSummary(props) {
   console.log(seatType, seatPrice);
 
   return (
-    <div className="rounded-xl flex">
+    <div className="rounded-xl flex justify-center">
       <img className="object-cover" src={bg} alt="" />
-      <div className="absolute right-0 z-10">
-        <div className=" flex-col justify-between px-12 py-4">
+      <div className="absolute items-center justify-center z-10">
+        <div className="flex flex-col justify-between py-16">
           <div className="flex flex-col justify-between h-full font-poppins text-white">
-            <div className="text-4xl items-center font-semibold">ORDER SUMMARY</div>
+            <div className="text-4xl items-center font-semibold">
+              ORDER SUMMARY
+            </div>
             <hr className="border-1 bg-gray-300 h-px my-4" />
             <div className="flex justify-between items-center">
               <div className="text-2xl font-medium">SELECTED SEATS :</div>
               <div className="text-2xl font-base flex items-center">
-                <div className="text-base text-dimgray-200">({seats.length})</div>
+                <div className="text-base text-dimgray-200">
+                  ({seats.length})
+                </div>
                 <div className="ml-1">
                   (
                   {seats.map((seat, index) => {
@@ -62,7 +66,9 @@ export default function OrderSummary(props) {
                   <div className="text-2xl font-base flex items-center">
                     <div className="text-base text-dimgray-200">
                       ({seatType.budget > 0 ? `${seatType.budget}*250` : ""}
-                      {seatType.budget === 0 || seatType.elite === 0 ? " " : " +  "}
+                      {seatType.budget === 0 || seatType.elite === 0
+                        ? " "
+                        : " +  "}
                       {seatType.elite > 0 ? `${seatType.elite}*350` : ""})
                     </div>
                     <div className="ml-1">{seatPrice}</div>
@@ -92,10 +98,11 @@ export default function OrderSummary(props) {
               </div>
 
               <hr className="border-1 bg-gray-300 h-px my-4" />
-              <div className="flex items-center justify-center w-2/5 m-auto">
-                <div className=" rounded-xl [background:linear-gradient(90.57deg,#628eff,#8740cd_53.13%,#580475)] w-full py-2 mb-2 ">
-                  <div className=" py-1 text-center text-5xl font-semibold cursor-pointer">
-                    Place Order
+              <div className="flex items-center justify-center w-1/2 m-auto">
+                <div
+                  className=" rounded-xl [background:linear-gradient(90.57deg,#628eff,#8740cd_53.13%,#580475)] w-full py-2 mb-2 ">
+                  <div className="py-1 text-center text-5xl font-semibold cursor-pointer">
+                    <button>Place Order</button>
                   </div>
                 </div>
               </div>
@@ -103,6 +110,6 @@ export default function OrderSummary(props) {
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
